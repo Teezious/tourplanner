776a9d45e3f5a88156b6ec3c5f88eb94eb7f940b
Matthias Sparr <mp.sparr@aon.at>
2021-06-06 22:41:03 +0200

    made UI Components resizeable

--- tourplanner/src/main/java/at/matthias/tourplanner/BL/Maphandler.java
+++ tourplanner/src/main/java/at/matthias/tourplanner/BL/Maphandler.java
@@ -12,63 +12,64 @@ import javax.imageio.ImageIO;
 import org.apache.log4j.Logger;
 
 public class Maphandler {
-    private String url;
-    private String key;
+  private String url;
+  private String key;
 
-    private static final Logger logger = Logger.getLogger(Maphandler.class);
+  private static final Logger logger = Logger.getLogger(Maphandler.class);
 
-    public Maphandler() {
-        XMLReader reader = new XMLReader();
-        String path = reader.getFullPath("mapquest");
-        this.key = reader.readXMLElement(path, "key");
-        this.url = reader.readXMLElement(path, "url");
-    }
+  public Maphandler() {
+    XMLReader reader = new XMLReader();
+    String path = reader.getFullPath("mapquest");
+    this.key = reader.readXMLElement(path, "key");
+    this.url = reader.readXMLElement(path, "url");
+  }
 
-    public RouteItem requestHandler(String start, String end, String imagePath) {
-        logger.info("new Map- and directionRequest");
-        APIcomm client = new APIcomm();
-        var request = createDirectionsURL(start, end);      // TODO Split this
-        RouteItem route = client.directionRequest(request); // TODO Split this
-        request = getMapURL(route.getSessionId());
-        byte[] byteimg = client.imageRequest(request);
-        if (byteimg != null) {
-            try {
+  public RouteItem requestHandler(String start, String end, String imagePath) {
+    logger.info("new Map- and directionRequest");
+    APIcomm client = new APIcomm();
+    var request = createDirectionsURL(start, end); // TODO Split this
+    RouteItem route = client.directionRequest(request); // TODO Split this
+    request = getMapURL(route.getSessionId());
+    byte[] byteimg = client.imageRequest(request);
+    if (byteimg != null) {
+      try {
+        BufferedImage image = ImageIO.read(new ByteArrayInputStream(byteimg));
 
-                BufferedImage image = ImageIO.read(new ByteArrayInputStream(byteimg));
-
-                if (image != null) {
-                    var imgFile = new File(imagePath);
-                    ImageIO.write(image, "JPG", imgFile);
-                } else {
-                    logger.warn("Image is null");
-                }
-            }
-
-            catch (IOException e) {
-                logger.error("Error writing Image!" + e);
-            }
+        if (image != null) {
+          var imgFile = new File(imagePath);
+          ImageIO.write(image, "JPG", imgFile);
+        } else {
+          logger.warn("Image is null");
         }
+      }
 
-        return route;
+      catch (IOException e) {
+        logger.error("Error writing Image!" + e);
+      }
     }
 
-    public URL createDirectionsURL(String start, String end) {
-        URL request = null;
-        try {
-            request = new URL(this.url + "/directions/v2/route?key=" + this.key + "&from=" + start + "&to=" + end + "&unit=k");
-        } catch (MalformedURLException e) {
-            logger.error("Error creating Directions URL!" + e);
-        }
-        return request;
-    }
+    return route;
+  }
 
-    public URL getMapURL(String sessionID) {
-        URL mapurl = null;
-        try {
-            mapurl = new URL(this.url + "/staticmap/v5/map?key=" + this.key + "&session=" + sessionID + "&size=600,400");
-        } catch (MalformedURLException e) {
-            logger.error("Error creating Map URL!" + e);
-        }
-        return mapurl;
+  public URL createDirectionsURL(String start, String end) {
+    URL request = null;
+    try {
+      request = new URL(this.url + "/directions/v2/route?key=" + this.key + "&from=" + start
+          + "&to=" + end + "&unit=k");
+    } catch (MalformedURLException e) {
+      logger.error("Error creating Directions URL!" + e);
     }
+    return request;
+  }
+
+  public URL getMapURL(String sessionID) {
+    URL mapurl = null;
+    try {
+      mapurl = new URL(this.url + "/staticmap/v5/map?key=" + this.key + "&session=" + sessionID
+          + "&size=400,400");
+    } catch (MalformedURLException e) {
+      logger.error("Error creating Map URL!" + e);
+    }
+    return mapurl;
+  }
 }
