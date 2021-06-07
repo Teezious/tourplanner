2c566130f04a40d66f26e926733d4a1aaff1c6eb
Matthias Sparr <mp.sparr@aon.at>
2021-06-07 15:24:32 +0200

    forgot

--- tourplanner/src/main/java/at/matthias/tourplanner/BL/Reporthandler.java
+++ tourplanner/src/main/java/at/matthias/tourplanner/BL/Reporthandler.java
@@ -32,7 +32,6 @@ import java.util.Collections;
 import java.util.List;
 import javax.imageio.ImageIO;
 import org.apache.log4j.Logger;
-import org.apache.log4j.helpers.Loader;
 import org.jfree.chart.JFreeChart;
 
 public class Reporthandler {
